5a29487e773d565ac29b65816e0687dca92037fe
Gianluca Masone <Gianluca Masone>
2017-01-24 12:34:59 +0100

    added interface for the picker writer

---
+++ Contracts/IoTWork.Contracts/IIoTWriter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTWork.Contracts
+{
+    public interface IIoTWriter
+    {
+        void Init(String Parameter);
+
+        void Write(DateTime SentAt, DateTime ReadAt, Int64 SequenceNumber, string SourceAdrress, Int32 Network, Int32 Region, Int32 Ring, Int32 Device, Int32 Sensor, string DeviceUniqueAddress, IIoTSample Sample);
+    }
+}
